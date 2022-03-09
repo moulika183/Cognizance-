d433caaeddb69e34a02385c144e768a003da0c28
Tharaniesh <99646275+Tharaniesh3@users.noreply.github.com>
2022-03-09 16:55:37 +0530

    Update Qn1.py

--- Task8/Qn1.py
+++ Task8/Qn1.py
@@ -1,7 +1,7 @@
-impot numpy as np 
+import numpy as np 
 a = np.array([10,11,12,13,14])
 print(a)
 nz = 5
 z = np.zeros(len(a) + (len(a)-1)*(nz))
 z[::nz+1] = a
-print()np.floor(z)]
+print(np.floor(z))
